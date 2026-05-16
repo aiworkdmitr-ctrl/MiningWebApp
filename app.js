d325730d8b928994488a0b684d962195b5e346e2
aiworkdmitr-ctrl <aiworkdmitr@gmail.com>
2026-05-16 12:52:46 +0000

    Добавил интеграцию с TADS

--- app.js
+++ app.js
@@ -6,6 +6,30 @@ if (user) {
     } else {
         document.getElementById('message').innerText = 'Открой меня через Telegram-бот';
     }
-    document.getElementById('myButton').addEventListener('click', function() {
-        tg.showAlert('Ты активировал кнопку!');
+document.addEventListener('DOMContentLoaded', function() {
+
+var adButton = document.getElementById('rewardAdBtn');
+if (adButton) {
+adButton.addEventListener('click', function() {
+if (typeof TadsWidget === 'undefined') {
+alert('Рекламв временно недоступна');
+return;
+}
+
+TadsWidget.showRewarded({
+widgetId: "9726",
+onReward: function() {
+
+alert('Спасибо за просмотр,награда начислена')
+
+},
+onClose: function() {
+console.log('Реклама закрыта без награды');
+}
+});
+
+
+});
+}
+
 });
